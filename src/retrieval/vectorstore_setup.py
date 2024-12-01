ca14c79a2bce184d860a6b175fc676c1e881420b
Purdea Bogdan <bogdanpurdea14@gmail.com>
2024-12-01 11:11:47 +0200

    Swapped from NomicEmbeddings to OllamaEmbeddings to enable ollama GPU capabilities, embeddings model stays the same

--- src/retrieval/vectorstore_setup.py
+++ src/retrieval/vectorstore_setup.py
@@ -1,7 +1,8 @@
 from langchain_community.vectorstores import SKLearnVectorStore
-from langchain_nomic.embeddings import NomicEmbeddings
+from langchain_ollama import OllamaEmbeddings
+import os
 
-def setup_vectorstore(doc_splits, model_name="nomic-embed-text-v1.5"):
+def setup_vectorstore(doc_splits, model_name="nomic-embed-text"):
     """
     Sets up a vector store and retriever from split documents.
 
@@ -10,13 +11,31 @@ def setup_vectorstore(doc_splits, model_name="nomic-embed-text-v1.5"):
         model_name (str): The name of the embedding model.
 
     Returns:
-        retriever: The retriever instance from the vector store.
+        vector store: The vector store instance using the document splits and embedding model provided.
     """
     print("---INITIALIZING VECTORSTORE---")
 
     vectorstore = SKLearnVectorStore.from_documents(
         documents=doc_splits,
-        embedding=NomicEmbeddings(model=model_name, inference_mode="local"),
+        embedding=OllamaEmbeddings(model=model_name),
     )
-
     return vectorstore
+
+
+# TO DO - VERIFY AND FIX LOCAL LOADING OF VECTORSTORE
+def load_vectorstore(doc_splits, model_name = "nomic-embed-text-v1.5"):
+    
+    vectorstore_path = "path/to/vectorstore.pkl"  # Path to save/load the vectorstore
+
+    # Check if the vectorstore already exists locally
+    if os.path.exists(vectorstore_path):
+        vector_store = SKLearnVectorStore(
+            embedding=NomicEmbeddings(model=model_name, inference_mode="local"),
+            persist_path=vectorstore_path,
+        )
+    else:
+        print("Initializing new vectorstore...")
+        vectorstore = setup_vectorstore(doc_splits, model_name)
+        # Save the vectorstore locally for future use
+        vectorstore.persist()
+        print(f"Vector store saved to {vectorstore_path}")
